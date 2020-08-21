5e9a5f4bb129e788a9362ffa69065698fb3e42c0
Mike Manchee <mikemanchee@gmail.com>
2020-08-21 08:36:13 -0700

    complete test spec 1 to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+function robotics(input) {
+
+  let robArray = [];
+  for (let i = 0; i <= input; i++) {
+    robArray.push(i);
+  }
+  return robArray;
+}
+
+
+
 $(document).ready(function() {
   $("#robot-form").submit(function(event) {
-    let input1 = $("input#input1").val();
+    event.preventDefault();
+    let input1 = parseInt($("input#input1").val());
 
 
-    let result = robotics(input);
+    let result = robotics(input1);
     $("#result1").text(result);
   });
 });
