c5bf9b9af206d8541a7166cbd6102a09513a6e5f
Mike Manchee <mikemanchee@gmail.com>
2020-08-21 10:15:33 -0700

    cleanup the js code on the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,37 @@
+// Business Logic
 function robotics(input) {
-  let robArray = [];
-  let test;
+  let robotArray = [];
+  let indexOutput;
   
-  for (let i = 0; i <= input; i++) {
-    if (/[3]+/g.test(i)) {
-      test = "Won't you be my neighbor?";
-    } else if (/[2]+/g.test(i)) {
-      test = "Boop!";
-    } else if (/[1]+/g.test(i)) {
-      test = "Beep!";
+  for (let i = 0; i <= input; i++) {    
+    if (/[3]+/g.test(i)) {              // Test 5 
+      indexOutput = " Won't you be my neighbor?";
+    } else if (/[2]+/g.test(i)) {       // Test 4
+      indexOutput = " Boop!";
+    } else if (/[1]+/g.test(i)) {       // Test 3
+      indexOutput = " Beep!";
     } else {
-      test = i;
+      indexOutput = " " + i;
     }
-    robArray.push(test);    
+    robotArray.push(indexOutput);       // Test 2
   }
 
-  return robArray;
+  return robotArray;
 }
 
-
-
+// Interface Logic
 $(document).ready(function() {
   $("#robot-form").submit(function(event) {
     event.preventDefault();
     let input1 = $("input#input1").val();
-    let result;
+    let result1;
 
-    if (isNaN(input1)) {
-      result = "Mr. Roboger can only receive numbers.";
+    if (isNaN(input1)) {                // Test 1
+      result1 = "Mr. Roboger can only receive numbers.";
     } else {
-      result = robotics(parseInt(input1));
+      result1 = robotics(parseInt(input1));
     };
 
-    $("#result1").text(result);
+    $("#result1").text(result1);
   });
 });
