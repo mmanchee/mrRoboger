32a46a0aaf625421d48e9bda001f56ef5f1e5f67
Mike Manchee <mikemanchee@gmail.com>
2020-08-21 09:11:18 -0700

    complete test spec 2 to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 function robotics(input) {
-
   let robArray = [];
+  let test;
+  
   for (let i = 0; i <= input; i++) {
-    robArray.push(i);
+    if (/[1]+/g.test(i)) {
+      test = "Beep!";
+    } else {
+      test = i;
+    }
+    robArray.push(test);    
   }
+
   return robArray;
 }
 
@@ -12,10 +19,15 @@ function robotics(input) {
 $(document).ready(function() {
   $("#robot-form").submit(function(event) {
     event.preventDefault();
-    let input1 = parseInt($("input#input1").val());
+    let input1 = $("input#input1").val();
+    let result;
 
+    if (isNaN(input1)) {
+      result = "Mr. Roboger can only receive numbers.";
+    } else {
+      result = robotics(parseInt(input1));
+    };
 
-    let result = robotics(input1);
     $("#result1").text(result);
   });
 });
